a72b1555e2a3d96fbc4e479962f935d3a881fdf1
thirukkumaran-student <thirukkumaran2410584@ssn.edu.in>
2026-06-25 17:37:35 +0530

    Routing enabled

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ function Home(){
             <h1 className="text-3xl">Search for the GitHub Profile Here:</h1>
             <form className="w-full max-w-md h-20 shadow-md/30 font-mono bg-mauve-100 rounded-2xl flex justify-center items-center">
                 <input className = 'inset-shadow-sm/20 pl-2 bg-white w-xs h-9' placeholder="Type the profile id" type="search" value={username}onChange={(e) => {setName(e.target.value)}} id="" />
-                <button className="bg-green-600 w-20 h-9 ml-2 rounded-sm text-amber-50 text-xl hover:bg-green-700 transition-all duration-100" onClick={() => {navigate('/user/:username')}}>Search</button>
+                <button className="bg-green-600 w-20 h-9 ml-2 rounded-sm text-amber-50 text-xl hover:bg-green-700 transition-all duration-100" onClick={() => {navigate(`/user/${username}`)}}>Search</button>
             </form>
         </div>
     )
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,8 @@
 import '../styles/Profile.css'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 function Profile(){
     const {username} = useParams<{username : string}>()
-    
+    const navigate = useNavigate()
     if(!username){
         return (
             <h1>User not Found</h1>
@@ -10,10 +10,15 @@ function Profile(){
     }
     
     return (
-        <>
-        <h1>Profile</h1>
-        <h1>{username}</h1>
-        </>
+        <div className = 'flex justify-center items-center min-w-screen h-screen '>
+            <div className='w-sm flex items-center flex-col'>
+                <div className='shadow-xl/30 w-sm mb-3 rounded-md p-3.5'>
+                    
+                    <h1>{username}</h1>
+                </div>
+                <button className='w-16 h-9 text-gray-300 bg-gray-700 rounded-lg border' onClick = {() => {navigate('/')}}>back</button>
+            </div>
+        </div>
     )
 }
 
