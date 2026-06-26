bc065e710101ff0034a7e847ecbf1d64f22942b0
thirukkumaran-student <thirukkumaran2410584@ssn.edu.in>
2026-06-26 05:52:44 +0530

    Data is assigned, Design to be completed

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -68,10 +68,28 @@ function Profile(){
     return (
         <div className = 'flex justify-center items-center min-w-screen h-screen '>
             <div className='w-sm flex items-center flex-col'>
-                <div className='shadow-xl/30 w-sm mb-3 rounded-md p-3.5'>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                <div className='flex justify-center items-center flex-col shadow-xl/30 w-xs gap-2 mb-3 rounded-md p-3.5'>
+                    <div className='flex justify-center items-center w-full flex-col'>
+                        <img src={user?.avatar_url} className = 'min-w-28 rounded-4xl' alt="" />
+                        <h1>{user?.name}</h1>
+                        <h1>{user?.login}</h1>
+                        <p>{user?.bio}</p>
+                    </div>
+                    <div className='flex justify-center items-center flex-col'>
+                        <span className='flex justify-between'>
+                            <h1>Followers:</h1><h1>{user?.followers}</h1>
+                        </span>
+                        <span className='flex justify-between'>
+                            <h1>Following:</h1><h1>{user?.following}</h1>
+                        </span>
+                        <span className='flex justify-between'>
+                            <h1>Public repos:</h1><h1>{user?.public_repos}</h1>
+                        </span>
+                        <div className='flex flex-col justify-center items-center'>
+                        <h1>{user?.company}</h1>
+                        <h1>{user?.location}</h1>
+                        </div>
+                    </div>
                 </div>
                 <button className='w-16 h-9 text-gray-300 bg-gray-700 rounded-lg border' onClick = {() => {navigate('/')}}>back</button>
             </div>
